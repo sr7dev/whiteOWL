1d7fd7453ccc675d78298a12bcc5001f50b0cef6
calmcalmdown <44122665+calmcalmdown@users.noreply.github.com>
2019-05-17 13:33:32 +0200

    Update index.php

--- themes/whiteowl/index.php
+++ themes/whiteowl/index.php
@@ -46,11 +46,7 @@ get_header(); ?>
 
 				endwhile;
 
-				the_posts_pagination( array(
-					'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-				) );
+				
 
 			else :
 
