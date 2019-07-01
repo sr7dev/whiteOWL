4c718bbb09f0f08a8b52cca53ac8419ba97d8218
calmcalmdown <44122665+calmcalmdown@users.noreply.github.com>
2019-07-01 16:59:34 +0200

    Update single.php

--- themes/whiteowl/single.php
+++ themes/whiteowl/single.php
@@ -9,13 +9,10 @@
  * @since 1.0
  * @version 1.0
  */
-
 get_header(); ?>
-
 <div class="wrap">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
@@ -39,5 +36,4 @@ get_header(); ?>
 	</div><!-- #primary -->
 	<?php get_sidebar(); ?>
 </div><!-- .wrap -->
-
 <?php get_footer();
