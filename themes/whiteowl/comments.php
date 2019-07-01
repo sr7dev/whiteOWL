e7e9ee887df57a5ed99e5774ef48a4da7fd58d14
calmcalmdown <44122665+calmcalmdown@users.noreply.github.com>
2019-07-01 16:57:12 +0200

    Update comments.php

--- themes/whiteowl/comments.php
+++ themes/whiteowl/comments.php
@@ -22,9 +22,7 @@ if ( post_password_required() ) {
 	return;
 }
 ?>
-
 <div id="comments" class="comments-area">
-
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
